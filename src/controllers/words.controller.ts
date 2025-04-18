fc371e52adf4702efdd54ba6fdaed65761b119b8
lazalajochy <rosario.jose.dev@gmail.com>
2025-04-18 12:43:19 -0400

    find all

--- src/controllers/words.controller.ts
+++ src/controllers/words.controller.ts
@@ -1,18 +1,22 @@
 import { Response, Request } from 'express'
-import { createEntity } from '../services/entity.service'
+import * as entityService from '../services/entity.service'
 import { Word } from '../models/words.mode'
 import logger from '../utils/logger'
 
 export const words = async (req: Request, res: Response) => {
-  res.status(200).json({
-    message: 'Hello from words controller!'
-  })
+  try {
+    const words = await entityService.findEntity(Word)
+    res.status(200).json({ data: words })
+  } catch (error) {
+    logger.error('Error fetching words:', error)
+    res.status(500).json({ message: 'Internal server error' }) 
+  }
 }
 
 export const addWord = async (req: Request, res: Response) => {
   const { word } = req.body
   try {
-     const entity = await createEntity(Word, { word })
+     const entity = await entityService.createEntity(Word, { word })
     res.status(201).json({ data: entity })
   } catch (error) {
     logger.error('Error creating word:', error)
--- src/services/entity.service.ts
+++ src/services/entity.service.ts
@@ -6,3 +6,12 @@ export const createEntity = async (model: { new (data: any): any }, data: any) =
     throw new Error('Error creating entity')
   }
 }
+
+
+export const findEntity = async (model: { findAll: () => Promise<any> }) => {
+  try {
+    return await model.findAll();
+  } catch (error) {
+    throw new Error('Error finding entity');
+  }
+}
