df3e4cecdea34da9f9edd8b3eb4088a7008903ef
lazalajochy <rosario.jose.dev@gmail.com>
2025-04-18 12:36:19 -0400

    refactor entity.service

--- src/services/entity.service.ts
+++ src/services/entity.service.ts
@@ -1,9 +1,8 @@
-import logger from "../utils/logger"
+
 export const createEntity = async (model: { new (data: any): any }, data: any) => {
   try {
     return new model(data).save();
   } catch (error) {
-    logger.error('Error creating entity:', error)
     throw new Error('Error creating entity')
   }
 }
